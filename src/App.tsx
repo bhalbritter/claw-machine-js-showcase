5fc888452fdd97ca3abf1854e582ce551fa8e8c5
benedikthalbritter <bene.halbo@gmail.com>
2024-12-07 15:08:07 +0100

    added and run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-
 function App() {
-
-  return (
-      <>
-          <h1 className="bg-amber-300">
-              Helasdflo world!
-          </h1>
-      </>
-  )
+	return (
+		<>
+			<h1 className="bg-amber-300">Helasdflo world!</h1>
+		</>
+	)
 }
 
 export default App
